89c3ceb7ba94aa961700342edab75910ff9927ef
vkashegde <vikas@kofluence.com>
2024-03-19 10:52:05 +0530

    initial

---
+++ lib/view/authScreens/mobile_login_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MobileLoginScreen extends StatefulWidget {
+  const MobileLoginScreen({super.key});
+
+  @override
+  State<MobileLoginScreen> createState() => _MobileLoginScreenState();
+}
+
+class _MobileLoginScreenState extends State<MobileLoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
